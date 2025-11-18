a2c7fdb0fe5e8531967fa0432b95142d487b9a33
Ushnika09 <karushnika@gmail.com>
2025-11-18 09:56:01 +0530

    completed

--- script.js
+++ script.js
@@ -21,3 +21,41 @@ toggleBtn.addEventListener("click", () => {
     localStorage.setItem("theme", "dark");
   }
 });
+
+
+const typedTextSpan = document.querySelector(".typed-text");
+
+const textArray = ["Full Stack Developer", "Web Developer", "Fron-end Developer", "Back-end Developer", "MERN Stack Developer", "Learner"];
+
+
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex <textArray[textArrayIndex].length){
+    typedTextSpan.textContent+=textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  }else{
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if(charIndex>0){
+    typedTextSpan.textContent =textArray[textArrayIndex].substring(0,charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  }else{
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
